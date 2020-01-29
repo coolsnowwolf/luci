a94d7ecd183fbfba76fd4bdacf75d63386f09201
Twinzo1 <1282055288@qq.com>
2020-01-29 09:48:06 +0800

    修复添加设备备注信息

--- modules/luci-base/luasrc/tools/status.lua
+++ modules/luci-base/luasrc/tools/status.lua
@@ -7,6 +7,7 @@ local uci = require "luci.model.uci".cursor()
 
 local function dhcp_leases_common(family)
 	local rv = { }
+	local rc = { }
 	local nfs = require "nixio.fs"
 	local leasefile = "/tmp/dhcp.leases"
 
@@ -25,7 +26,30 @@ local function dhcp_leases_common(family)
 			if not ln then
 				break
 			else
-				local ts, mac, ip, name, comments, duid = ln:match("^(%d+) (%S+) (%S+) (%S+) (%S+) (%S+)")
+				local ts, mac, ip, name, duid = ln:match("^(%d+) (%S+) (%S+) (%S+) (%S+)")
+				local comments
+				local fc = io.open("/tmp/dhcp_comments.conf", "r")
+				if fc then
+					local lc
+					while true do
+						lc =  fc:read("*l")
+						if not lc then break end
+						local comment, macs, ips = lc:match("(%S+) (%S+) (%S+)")		
+						if macs ~= "-" then
+							if macs == mac then 
+								comments = comment
+								break
+							end
+						else
+							if ips == ip then
+								comments = comment
+								break
+							end
+						end
+					end
+				end
+				fc:close()
+				
 				local expire = tonumber(ts) or 0
 				if ts and mac and ip and name and duid then
 					if family == 4 and not ip:match(":") then
