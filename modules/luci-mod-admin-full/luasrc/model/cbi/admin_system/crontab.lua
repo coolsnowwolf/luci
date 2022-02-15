d9baa31e32cc3d775297f1c4cd2e281fed9e9a54
ZeaKyX <43315078+ZeaKyX@users.noreply.github.com>
2022-02-15 21:37:19 +0800

    Crontab: Avoid some formatting issues (#87)
    Refer to Upstream

--- modules/luci-mod-admin-full/luasrc/model/cbi/admin_system/crontab.lua
+++ modules/luci-mod-admin-full/luasrc/model/cbi/admin_system/crontab.lua
@@ -4,6 +4,7 @@
 
 local fs = require "nixio.fs"
 local cronfile = "/etc/crontabs/root" 
+local util = require "luci.util"
 
 f = SimpleForm("crontab", translate("Scheduled Tasks"),
 	translate("This is the system crontab in which scheduled tasks can be defined.") ..
@@ -20,7 +21,7 @@ end
 function f.handle(self, state, data)
 	if state == FORM_VALID then
 		if data.crons then
-			fs.writefile(cronfile, data.crons:gsub("\r\n", "\n"))
+			fs.writefile(cronfile, util.trim(data.crons):gsub("\r\n", "\n") .. "\n")
 			luci.sys.call("/usr/bin/crontab %q" % cronfile)
 		else
 			fs.writefile(cronfile, "")
