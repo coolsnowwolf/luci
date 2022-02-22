e9605b4cd77842a3011db0df2f7a51970e52c686
gaosen <gaosen@tuya.com>
2022-02-22 10:25:20 +0800

    luci-proto-vxlan: add new package

---
+++ protocols/luci-proto-vxlan/luasrc/model/network/proto_vxlan6.lua
@@ -0,0 +1,38 @@
+
+local netmod = luci.model.network
+local interface = luci.model.network.interface
+local proto = netmod:register_protocol("vxlan6")
+
+function proto.get_i18n(self)
+	return luci.i18n.translate("VXLANv6 (RFC7348)")
+end
+
+function proto.ifname(self)
+	return "vxlan-" .. self.sid
+end
+
+function proto.opkg_package(self)
+	return "vxlan"
+end
+
+function proto.is_installed(self)
+	return nixio.fs.access("/lib/netifd/proto/vxlan.sh")
+end
+
+function proto.is_floating(self)
+	return true
+end
+
+function proto.is_virtual(self)
+	return true
+end
+
+function proto.get_interfaces(self)
+	return nil
+end
+
+function proto.contains_interface(self, ifc)
+	 return (netmod:ifnameof(ifc) == self:ifname())
+end
+
+netmod:register_pattern_virtual("^vxlan%-%w")
