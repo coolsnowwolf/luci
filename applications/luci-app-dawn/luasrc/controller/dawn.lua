314af2d898f234b7897ac691296afc753f6e5bbc
zhengmz <zhengmz@flowsea.com>
2022-04-25 14:38:57 +0800

    move dawn to network

--- applications/luci-app-dawn/luasrc/controller/dawn.lua
+++ applications/luci-app-dawn/luasrc/controller/dawn.lua
@@ -1,10 +1,11 @@
 module("luci.controller.dawn", package.seeall)
 
 function index()
-    local e = entry({ "admin", "dawn" }, firstchild(), "DAWN", 60)
+    local e = entry({ "admin", "network", "dawn" }, alias("admin", "network", "dawn", "view_network"), _("Configure DAWN"), 60)
+    e.i18n = "dawn"
     e.dependent = false
     e.acl_depends = { "luci-app-dawn" }
 
-    entry({ "admin", "dawn", "view_network" }, cbi("dawn/dawn_network"), "View Network Overview", 1)
-    entry({ "admin", "dawn", "view_hearing_map" }, cbi("dawn/dawn_hearing_map"), "View Hearing Map", 2)
+    entry({ "admin", "network", "dawn", "view_network" }, cbi("dawn/dawn_network"), "View Network Overview", 1).leaf = true
+    entry({ "admin", "network", "dawn", "view_hearing_map" }, cbi("dawn/dawn_hearing_map"), "View Hearing Map", 2).leaf = true
 end
