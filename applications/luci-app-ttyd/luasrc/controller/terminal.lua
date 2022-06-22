95da1b6d0b9a63ae417e1292ea5256ee07a27ed2
Beginner-Go <70857188+Beginner-Go@users.noreply.github.com>
2022-06-22 16:43:33 +0000

    luci-app-ttyd: code alignment

--- applications/luci-app-ttyd/luasrc/controller/terminal.lua
+++ applications/luci-app-ttyd/luasrc/controller/terminal.lua
@@ -4,6 +4,6 @@ function index()
 	if not (luci.sys.call("pidof ttyd > /dev/null") == 0) then
 		return
 	end
-	
+
 	entry({"admin", "system", "terminal"}, template("terminal/terminal"), _("TTYD Terminal"), 10).leaf = true
 end
