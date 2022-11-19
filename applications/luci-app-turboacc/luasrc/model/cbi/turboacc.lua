c7fb30534a0a7c3a9c7d83866ca8d38ea538ad1d
coolsnowwolf <coolsnowwolf@gmail.com>
2022-11-20 00:14:21 +0800

    luci-app-turboacc: add fullcone mode 2 support

--- applications/luci-app-turboacc/luasrc/model/cbi/turboacc.lua
+++ applications/luci-app-turboacc/luasrc/model/cbi/turboacc.lua
@@ -47,11 +47,12 @@ bbr_cca.default = 0
 bbr_cca.description = translate("Using BBR CCA can improve TCP network performance effectively")
 end
 
-if nixio.fs.access("/lib/modules/" .. kernel_version .. "/xt_FULLCONENAT.ko") then
-fullcone_nat = s:option(Flag, "fullcone_nat", translate("FullCone NAT"))
+fullcone_nat = s:option(ListValue, "fullcone_nat", translate("FullCone NAT"))
 fullcone_nat.default = 0
+fullcone_nat:value("0", translate("Disable"))
+fullcone_nat:value("1", translate("Compatible Mode"))
+fullcone_nat:value("2", translate("High Performing Mode"))
 fullcone_nat.description = translate("Using FullCone NAT can improve gaming performance effectively")
-end
 
 if nixio.fs.access("/usr/sbin/pdnsd") or nixio.fs.access("/usr/bin/dnsforwarder") or nixio.fs.access("/usr/bin/dnsproxy") then
 dns_caching = s:option(Flag, "dns_caching", translate("DNS Caching"))
