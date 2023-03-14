5010b444c8a0ac4efa4f52f40b1e35623b59cb80
coolsnowwolf <coolsnowwolf@gmail.com>
2023-03-14 17:40:08 +0800

    luci-newapi: change display dbm view

--- modules/luci-newapi/root/etc/newapi/usr/lib/lua/luci/model/cbi/admin_network/wifi.lua
+++ modules/luci-newapi/root/etc/newapi/usr/lib/lua/luci/model/cbi/admin_network/wifi.lua
@@ -219,8 +219,8 @@ if hwtype == "mac80211" then
 
 		tp:value("", translate("auto"))
 		for _, p in ipairs(tx_power_list) do
-			tp:value(p.driver_dbm, "%i dBm (%i mW)"
-				%{ p.display_dbm, p.display_mw })
+			tp:value(p.driver_dbm, "%i dBm"
+				%{ p.display_dbm })
 		end
 	end
 
