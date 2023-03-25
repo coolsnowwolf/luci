983f6d0fa1ee2162dd1ce9c8392f088ca61c3a2c
qingtian110 <1335538902@qq.com>
2023-03-25 18:48:47 +0800

    luci-app-mosdns: update from upstream - mosdns v5 support

--- applications/luci-app-mosdns/root/usr/share/mosdns/mosdns.sh
+++ applications/luci-app-mosdns/root/usr/share/mosdns/mosdns.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+script_action=${1}
+
 logfile_path() (
 	configfile=$(uci -q get mosdns.config.configfile)
 	if [ "$configfile" = "/etc/mosdns/config.yaml" ]; then
@@ -11,7 +13,7 @@ logfile_path() (
 )
 
 interface_dns() (
-	if [ "$(uci -q get mosdns.config.custom_local_dns)" -eq 1 ]; then
+	if [ "$(uci -q get mosdns.config.custom_local_dns)" = 1 ]; then
 		uci -q get mosdns.config.local_dns
 	else
 		peerdns=$(uci -q get network.wan.peerdns)
@@ -23,21 +25,21 @@ interface_dns() (
 			echo $interface_status | jsonfilter -e "@['dns-server'][0]"
 			echo $interface_status | jsonfilter -e "@['dns-server'][1]"
 		fi
-		[ $? -ne 0 ] && echo "119.29.29.29"
+		[ $? -ne 0 ] && echo "119.29.29.29 223.5.5.5"
 	fi
 )
 
 ad_block() (
 	adblock=$(uci -q get mosdns.config.adblock)
-	if [ "$adblock" -eq 1 ]; then
+	if [ "$adblock" = 1 ]; then
 		ad_source=$(uci -q get mosdns.config.ad_source)
 		if [ "$ad_source" = "geosite.dat" ]; then
-			echo "provider:geosite:category-ads-all"
+			echo "/var/mosdns/geosite_category-ads-all.txt"
 		else
-			echo "provider:adlist"
+			echo "/etc/mosdns/rule/adlist.txt"
 		fi
 	else
-		echo "full:disable-category-ads-all.null"
+		touch /var/disable-ads.txt ; echo "/var/disable-ads.txt"
 	fi
 )
 
@@ -50,7 +52,7 @@ adlist_update() (
 		[ "$google_status" -ne "204" ] && mirror="https://ghproxy.com/"
 	fi
 	echo -e "\e[1;32mDownloading $mirror$ad_source\e[0m"
-	curl --connect-timeout 60 -m 90 --ipv4 -fSLo "$AD_TMPDIR/adlist.txt" "$mirror$ad_source"
+	curl --connect-timeout 60 -m 90 --ipv4 -kfSLo "$AD_TMPDIR/adlist.txt" "$mirror$ad_source"
 	if [ $? -ne 0 ]; then
 		rm -rf "$AD_TMPDIR"
 		exit 1
@@ -66,7 +68,7 @@ geodat_update() (
 		google_status=$(curl -I -4 -m 3 -o /dev/null -s -w %{http_code} http://www.google.com/generate_204)
 		[ "$google_status" -ne "204" ] && mirror="https://ghproxy.com/"
 		echo -e "\e[1;32mDownloading "$mirror"https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/$1\e[0m"
-		curl --connect-timeout 60 -m 900 --ipv4 -fSLo "$TMPDIR/$1" ""$mirror"https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/$1"
+		curl --connect-timeout 60 -m 900 --ipv4 -kfSLo "$TMPDIR/$1" ""$mirror"https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/$1"
 	)
 	TMPDIR=$(mktemp -d) || exit 1
 	geodat_download geoip.dat && geodat_download geosite.dat
@@ -78,14 +80,30 @@ geodat_update() (
 	rm -rf "$TMPDIR"
 )
 
-if [ "$1" == "dns" ]; then
-	interface_dns
-elif [ "$1" == "ad" ]; then
-	ad_block
-elif [ "$1" == "geodata" ]; then
-	geodat_update && adlist_update
-elif [ "$1" == "logfile" ]; then
-	logfile_path
-elif [ "$1" == "adlist_update" ]; then
-	adlist_update
-fi
+restart_service() {
+	service mosdns restart
+}
+
+case $script_action in
+	"dns")
+		interface_dns
+	;;
+	"ad")
+		ad_block
+	;;
+	"geodata")
+		geodat_update && adlist_update && restart_service
+	;;
+	"logfile")
+		logfile_path
+	;;
+	"adlist_update")
+		adlist_update && restart_service
+	;;
+	"version")
+		mosdns version
+	;;
+	*)
+		exit 0
+	;;
+esac
