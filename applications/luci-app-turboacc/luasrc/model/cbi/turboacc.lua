coolsnowwolf/luci

commit 57a991ada6b5690fe7f1293d39a55db23c0fc61a

luci-app-turboacc: fix hw_flow always true (#166)

applications/luci-app-turboacc/root/etc/init.d/turboacc
@@ -21,7 +21,7 @@ inital_conf(){
 	config_get "dns_caching_mode" "config" "dns_caching_mode" "0"
 	config_get "dns_caching_dns" "config" "dns_caching_dns"
 
-	[ -e "/lib/modules/$(uname -r)/mtkhnat.ko" ] && { sw_flow="0"; hw_flow="1"; }
+	[ ! -e "/lib/modules/$(uname -r)/mtkhnat.ko" ] && hw_flow="0";
 	[ ! -e "/lib/modules/$(uname -r)/xt_FLOWOFFLOAD.ko" ] && { sw_flow="0"; hw_flow="0"; }
 	[ ! -e "/lib/modules/$(uname -r)/tcp_bbr.ko" ] && bbr_cca="0"
 }

commit 1cd432ab19a2eadad14c7f8e86cdebbb96e6a40f

luci-app-turboacc: add mtk wed config support
Fixes: #224
Fixes: 57a991a ("luci-app-turboacc: fix hw_flow always true")

applications/luci-app-turboacc/luasrc/model/cbi/turboacc.lua
@@ -14,21 +14,22 @@ if nixio.fs.access("/lib/modules/" .. kernel_version .. "/xt_FLOWOFFLOAD.ko") th
 sw_flow = s:option(Flag, "sw_flow", translate("Software flow offloading"))
 sw_flow.default = 0
 sw_flow.description = translate("Software based offloading for routing/NAT")
-if nixio.fs.access("/lib/modules/" .. kernel_version .. "/mtkhnat.ko") then
-sw_flow:depends("hw_flow", 0)
-else
 sw_flow:depends("sfe_flow", 0)
-end
 end
 
 if luci.sys.call("cat /etc/openwrt_release | grep -Eq 'filogic|mt762' ") == 0 then
 hw_flow = s:option(Flag, "hw_flow", translate("Hardware flow offloading"))
 hw_flow.default = 0
-hw_flow.description = translate("Requires hardware NAT support. Implemented at least for mt762x")
-if nixio.fs.access("/lib/modules/" .. kernel_version .. "/mtkhnat.ko") then
-hw_flow:depends("sw_flow", 0)
-else
+hw_flow.description = translate("Requires hardware NAT support, implemented at least for mt762x")
 hw_flow:depends("sw_flow", 1)
+end
+
+if luci.sys.call("cat /etc/openwrt_release | grep -Eq 'mediatek' ") == 0 then
+if nixio.fs.access("/lib/modules/" .. kernel_version .. "/mt7915e.ko") then
+hw_wed = s:option(Flag, "hw_wed", translate("MTK WED WO offloading"))
+hw_wed.default = 0
+hw_wed.description = translate("Requires hardware support, implemented at least for Filogic 8x0")
+hw_wed:depends("hw_flow", 1)
 end
 end
 

applications/luci-app-turboacc/po/zh-cn/turboacc.po
@@ -19,8 +19,11 @@ msgstr "基于软件的 Routing/NAT 分载"
 msgid "Hardware flow offloading"
 msgstr "硬件流量分载"
 
-msgid "Requires hardware NAT support. Implemented at least for mt762x"
-msgstr "需要硬件 NAT 支持。目前 mt762x 已实现"
+msgid "Requires hardware NAT support, implemented at least for mt762x"
+msgstr "需要硬件 NAT 支持，目前 mt762x 已实现"
+
+msgid "Requires hardware support, implemented at least for Filogic 8x0"
+msgstr "需要硬件支持，目前 Filogic 8x0 已实现"
 
 msgid "Shortcut-FE flow offloading"
 msgstr "Shortcut-FE 流量分载"
@@ -67,7 +70,6 @@ msgstr "DNS 解析程序"
 msgid "Using PDNSD to query and cache"
 msgstr "使用 PDNSD 解析"
 
-
 msgid "Using DNSForwarder to query and cache"
 msgstr "使用 DNSForwarder 解析"
 
@@ -95,5 +97,5 @@ msgstr "全锥型 NAT"
 msgid "DNS Caching"
 msgstr "DNS 缓存"
 
-msgid "Open Web Interface"
-msgstr "打开 Web 界面"
+msgid "MTK WED WO offloading"
+msgstr "MTK 无线硬件加速"

applications/luci-app-turboacc/root/etc/config/turboacc
@@ -4,6 +4,7 @@ config turboacc 'config'
 	option hw_flow '1'
 	option sfe_flow '1'
 	option fullcone_nat '2'
+	option hw_wed '0'
 	option bbr_cca '0'
 	option dns_caching '0'
 	option dns_caching_mode '1'

applications/luci-app-turboacc/root/etc/init.d/turboacc
@@ -12,8 +12,9 @@ restart_utils="true"
 
 inital_conf(){
 	config_load "turboacc"
-	config_get "sw_flow" "config" "sw_flow" "0"
+	config_get "hw_wed" "config" "hw_wed" "0"
 	config_get "hw_flow" "config" "hw_flow" "0"
+	config_get "sw_flow" "config" "sw_flow" "0"
 	config_get "sfe_flow" "config" "sfe_flow" "0"
 	config_get "bbr_cca" "config" "bbr_cca" "0"
 	config_get "fullcone_nat" "config" "fullcone_nat" "0"
@@ -21,7 +22,6 @@ inital_conf(){
 	config_get "dns_caching_mode" "config" "dns_caching_mode" "0"
 	config_get "dns_caching_dns" "config" "dns_caching_dns"
 
-	[ ! -e "/lib/modules/$(uname -r)/mtkhnat.ko" ] && hw_flow="0";
 	[ ! -e "/lib/modules/$(uname -r)/xt_FLOWOFFLOAD.ko" ] && { sw_flow="0"; hw_flow="0"; }
 	[ ! -e "/lib/modules/$(uname -r)/tcp_bbr.ko" ] && bbr_cca="0"
 }
@@ -232,8 +232,30 @@ load_sfe() {
 }
 
 unload_sfe() {
-	[ -d /sys/module/shortcut_fe_cm ] && rmmod shortcut_fe_cm 
+	[ -d /sys/module/shortcut_fe_cm ] && rmmod shortcut_fe_cm
 	[ -d /sys/module/fast_classifier ] && rmmod fast_classifier
+}
+
+load_wed() {
+	local kernel_version=$(uname -r)
+
+	grep -Eq 'mediatek' /etc/openwrt_release && \
+	    ! grep -Eq 'mt7915e' /etc/modules.conf && \
+		[ -e "/lib/modules/$kernel_version/mt7915e.ko" ] && {
+			sed -i '$aoptions mt7915e wed_enable=Y' /etc/modules.conf
+			rmmod mt7915e && sleep 1 && modprobe mt7915e && wifi up
+		}
+}
+
+unload_wed() {
+	local kernel_version=$(uname -r)
+
+	grep -Eq 'mediatek' /etc/openwrt_release && \
+	    grep -Eq 'mt7915e' /etc/modules.conf && \
+		[ -e "/lib/modules/$kernel_version/mt7915e.ko" ] && {
+			sed -i '/mt7915e/d' /etc/modules.conf
+			rmmod mt7915e && sleep 1 && modprobe mt7915e && wifi up
+		}
 }
 
 start(){
@@ -244,12 +266,12 @@ start(){
 	uci set firewall.@defaults[0].fullcone="${fullcone_nat}"
 	uci commit firewall
 
-	[ "${sw_flow}" -eq "0" ] && [ "${hw_flow}" -eq "1" ] && {
-		lsmod | grep -q mtkhnat || modprobe mtkhnat
-	}
-
 	[ "${sw_flow}" -ne "1" ] && [ "${sfe_flow}" -eq "1" ] && {
 		load_sfe
+	}
+
+	[ "${hw_flow}" -eq "1" ] && [ "${hw_wed}" -eq "1" ] && {
+		load_wed
 	}
 
 	if [ "${bbr_cca}" -eq "1" ];  then
@@ -294,8 +316,8 @@ stop(){
 	uci set firewall.@defaults[0].fullcone="${fullcone_nat}"
 	uci commit firewall
 
-	[ "${hw_flow}" -eq "0" ] && {
-		lsmod | grep -q mtkhnat && rmmod mtkhnat
+	[ "${hw_wed}" -eq "0" ] && {
+		unload_wed
 	}
 
 	unload_sfe

applications/luci-app-turboacc/root/etc/uci-defaults/luci-turboacc
@@ -9,7 +9,7 @@ EOF
 
 [ -n "$(lsmod | grep mtkhnat)" ] && \
 	uci set turboacc.config.sw_flow='0' && \
-	uci set turboacc.config.hw_flow='1'
+	uci set turboacc.config.hw_flow='0'
 
 uci commit turboacc
 