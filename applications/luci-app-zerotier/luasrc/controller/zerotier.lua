968580767cec761a946351e680734c9d1706cb5a
vpei <vpei@live.com>
2023-12-04 23:43:35 +0800

    Upgrade from 0.3.0 to 0.4.1

--- applications/luci-app-zerotier/luasrc/controller/zerotier.lua
+++ applications/luci-app-zerotier/luasrc/controller/zerotier.lua
@@ -5,13 +5,13 @@ function index()
 		return
 	end
 
-	entry({"admin", "vpn"}, firstchild(), "VPN", 45).dependent = false
+	entry({"admin", "network"}, firstchild(), "network", 45).dependent = false
 
-	entry({"admin", "vpn", "zerotier"}, alias("admin", "vpn", "zerotier", "general"), _("ZeroTier"), 99)
-	entry({"admin", "vpn", "zerotier", "general"}, cbi("zerotier/settings"), _("Base Setting"), 1)
-	entry({"admin", "vpn", "zerotier", "log"}, form("zerotier/info"), _("Interface Info"), 2)
+	entry({"admin", "network", "zerotier"}, alias("admin", "network", "zerotier", "general"), _("ZeroTier"), 99)
+	entry({"admin", "network", "zerotier", "general"}, cbi("zerotier/settings"), _("Base Setting"), 1)
+	entry({"admin", "network", "zerotier", "log"}, form("zerotier/info"), _("Interface Info"), 2)
 
-	entry({"admin", "vpn", "zerotier", "status"}, call("act_status"))
+	entry({"admin", "network", "zerotier", "status"}, call("act_status"))
 end
 
 function act_status()
