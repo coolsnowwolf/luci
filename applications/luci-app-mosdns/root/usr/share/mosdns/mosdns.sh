b6a126829eeb79b3fb6abdbf1fd97308a1570be0
qingtian110 <1335538902@qq.com>
2024-03-26 20:51:50 +0800

    luci-app-mosdns: update from upstream
    Signed-off-by: qingtian110 <1335538902@qq.com>

--- applications/luci-app-mosdns/root/usr/share/mosdns/mosdns.sh
+++ applications/luci-app-mosdns/root/usr/share/mosdns/mosdns.sh
@@ -29,51 +29,80 @@ interface_dns() (
     fi
 )
 
-ad_block() (
+get_adlist() (
     adblock=$(uci -q get mosdns.config.adblock)
     if [ "$adblock" = 1 ]; then
+        mkdir -p /etc/mosdns/rule/adlist
         ad_source=$(uci -q get mosdns.config.ad_source)
-        if [ "$ad_source" = "geosite.dat" ]; then
-            echo "/var/mosdns/geosite_category-ads-all.txt"
-        else
-            echo "/etc/mosdns/rule/adlist.txt"
-        fi
+        for url in $ad_source;
+        do
+            if [ $(echo $url) = 'geosite.dat' ]; then
+                echo "        - \"/var/mosdns/geosite_category-ads-all.txt\""
+            elif echo "$url" | grep -Eq "^file://" ; then
+                echo "        - \"$(echo "$url" | sed 's/file:\/\///')\""
+            else
+                echo "        - \"/etc/mosdns/rule/adlist/$(basename $url)\""
+                [ ! -f "/etc/mosdns/rule/adlist/$(basename $url)" ] && touch /etc/mosdns/rule/adlist/$(basename $url)
+            fi
+        done
     else
-        touch /var/disable-ads.txt ; echo "/var/disable-ads.txt"
+        rm -rf /etc/mosdns/rule/adlist /etc/mosdns/rule/.ad_source
+        touch /var/disable-ads.txt
+        echo "        - \"/var/disable-ads.txt\""
     fi
 )
 
-adlist_update() (
+adlist_update() {
+    [ "$(uci -q get mosdns.config.adblock)" != 1 ] && return 0
+    lock_file=/var/lock/mosdns_ad_update.lock
     ad_source=$(uci -q get mosdns.config.ad_source)
-    [ "$ad_source" = "geosite.dat" ] || [ -z "$ad_source" ] && exit 0
     AD_TMPDIR=$(mktemp -d) || exit 1
-    if echo "$ad_source" | grep -Eq "^https://raw.githubusercontent.com" ; then
-        google_status=$(curl -I -4 -m 3 -o /dev/null -s -w %{http_code} http://www.google.com/generate_204)
-        [ "$google_status" -ne "204" ] && mirror="https://ghproxy.com/"
+    mirror=""
+    : > /etc/mosdns/rule/.ad_source
+    if [ -f "$lock_file" ]; then
+        has_update=0
+        exit 0
+    else
+        : > $lock_file
     fi
-    echo -e "\e[1;32mDownloading $mirror$ad_source\e[0m"
-    curl --connect-timeout 60 -m 90 --ipv4 -kfSLo "$AD_TMPDIR/adlist.txt" "$mirror$ad_source"
+    has_update=0
+    for url in $ad_source;
+    do
+        if [ "$url" != "geosite.dat" ] && [ $(echo "$url" | grep -c -E "^file://") -eq 0 ]; then
+            has_update=1
+            echo "$url" >> /etc/mosdns/rule/.ad_source
+            filename=$(basename $url)
+            if echo "$url" | grep -Eq "^https://raw.githubusercontent.com" ; then
+                [ -n "$(uci -q get mosdns.config.github_proxy)" ] && mirror="$(uci -q get mosdns.config.github_proxy)/"
+            fi
+            echo -e "\e[1;32mDownloading $mirror$url\e[0m"
+            curl --connect-timeout 5 -m 90 --ipv4 -kfSLo "$AD_TMPDIR/$filename" "$mirror$url"
+        fi
+    done
     if [ $? -ne 0 ]; then
+        echo -e "\e[1;31mRules download failed.\e[0m"
         rm -rf "$AD_TMPDIR"
         exit 1
     else
-        \cp "$AD_TMPDIR/adlist.txt" /etc/mosdns/rule/adlist.txt
-        echo "$ad_source" > /etc/mosdns/rule/.ad_source
-        rm -rf "$AD_TMPDIR"
+        [ $has_update -eq 1 ] && {
+            mkdir -p /etc/mosdns/rule/adlist
+            rm -rf /etc/mosdns/rule/adlist/*
+            \cp $AD_TMPDIR/* /etc/mosdns/rule/adlist
+        }
     fi
-)
+    rm -rf "$AD_TMPDIR" $lock_file
+}
 
 geodat_update() (
     TMPDIR=$(mktemp -d) || exit 1
-    google_status=$(curl -I -4 -m 3 -o /dev/null -s -w %{http_code} http://www.google.com/generate_204)
-    [ "$google_status" -ne "204" ] && mirror="https://ghproxy.com/"
+    [ -n "$(uci -q get mosdns.config.github_proxy)" ] && mirror="$(uci -q get mosdns.config.github_proxy)/"
     # geoip.dat - cn-private
     echo -e "\e[1;32mDownloading "$mirror"https://github.com/Loyalsoldier/geoip/releases/latest/download/geoip-only-cn-private.dat\e[0m"
-    curl --connect-timeout 60 -m 900 --ipv4 -kfSLo "$TMPDIR/geoip.dat" ""$mirror"https://github.com/Loyalsoldier/geoip/releases/latest/download/geoip-only-cn-private.dat"
+    curl --connect-timeout 5 -m 60 --ipv4 -kfSLo "$TMPDIR/geoip.dat" ""$mirror"https://github.com/Loyalsoldier/geoip/releases/latest/download/geoip-only-cn-private.dat"
     [ $? -ne 0 ] && rm -rf "$TMPDIR" && exit 1
     # checksum - geoip.dat
     echo -e "\e[1;32mDownloading "$mirror"https://github.com/Loyalsoldier/geoip/releases/latest/download/geoip-only-cn-private.dat.sha256sum\e[0m"
-    curl --connect-timeout 60 -m 900 --ipv4 -kfSLo "$TMPDIR/geoip.dat.sha256sum" ""$mirror"https://github.com/Loyalsoldier/geoip/releases/latest/download/geoip-only-cn-private.dat.sha256sum"
+    curl --connect-timeout 5 -m 10 --ipv4 -kfSLo "$TMPDIR/geoip.dat.sha256sum" ""$mirror"https://github.com/Loyalsoldier/geoip/releases/latest/download/geoip-only-cn-private.dat.sha256sum"
     [ $? -ne 0 ] && rm -rf "$TMPDIR" && exit 1
     if [ "$(sha256sum "$TMPDIR/geoip.dat" | awk '{print $1}')" != "$(cat "$TMPDIR/geoip.dat.sha256sum" | awk '{print $1}')" ]; then
         echo -e "\e[1;31mgeoip.dat checksum error\e[0m"
@@ -83,11 +112,11 @@ geodat_update() (
 
     # geosite.dat
     echo -e "\e[1;32mDownloading "$mirror"https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat\e[0m"
-    curl --connect-timeout 60 -m 900 --ipv4 -kfSLo "$TMPDIR/geosite.dat" ""$mirror"https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat"
+    curl --connect-timeout 5 -m 120 --ipv4 -kfSLo "$TMPDIR/geosite.dat" ""$mirror"https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat"
     [ $? -ne 0 ] && rm -rf "$TMPDIR" && exit 1
     # checksum - geosite.dat
     echo -e "\e[1;32mDownloading "$mirror"https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat.sha256sum\e[0m"
-    curl --connect-timeout 60 -m 900 --ipv4 -kfSLo "$TMPDIR/geosite.dat.sha256sum" ""$mirror"https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat.sha256sum"
+    curl --connect-timeout 5 -m 10 --ipv4 -kfSLo "$TMPDIR/geosite.dat.sha256sum" ""$mirror"https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat.sha256sum"
     [ $? -ne 0 ] && rm -rf "$TMPDIR" && exit 1
     if [ "$(sha256sum "$TMPDIR/geosite.dat" | awk '{print $1}')" != "$(cat "$TMPDIR/geosite.dat.sha256sum" | awk '{print $1}')" ]; then
         echo -e "\e[1;31mgeosite.dat checksum error\e[0m"
@@ -95,17 +124,12 @@ geodat_update() (
         exit 1
     fi
     rm -rf "$TMPDIR"/*.sha256sum
-    cp -f "$TMPDIR"/* /usr/share/v2ray
+    \cp -a "$TMPDIR"/* /usr/share/v2ray
     rm -rf "$TMPDIR"
 )
 
 restart_service() {
     /etc/init.d/mosdns restart
-}
-
-ecs_remote() {
-    ipaddr=$(curl -s --user-agent "curl/8.2.1" --connect-timeout 3 -H "Host:v4.ident.me" 49.12.234.183) || ipaddr=110.34.181.1
-    echo "ecs ${ipaddr%.*}.0/24"
 }
 
 flush_cache() {
@@ -123,8 +147,8 @@ v2dat_dump() {
     if [ "$configfile" = "/etc/mosdns/config.yaml" ]; then
         # default config
         v2dat unpack geoip -o /var/mosdns -f cn $v2dat_dir/geoip.dat
-        v2dat unpack geosite -o /var/mosdns -f cn -f 'geolocation-!cn' $v2dat_dir/geosite.dat
-        [ "$adblock" -eq 1 ] && [ "$ad_source" = "geosite.dat" ] && v2dat unpack geosite -o /var/mosdns -f category-ads-all $v2dat_dir/geosite.dat
+        v2dat unpack geosite -o /var/mosdns -f cn -f apple -f 'geolocation-!cn' $v2dat_dir/geosite.dat
+        [ "$adblock" -eq 1 ] && [ $(echo $ad_source | grep -c geosite.dat) -ge '1' ] && v2dat unpack geosite -o /var/mosdns -f category-ads-all $v2dat_dir/geosite.dat
     else
         # custom config
         v2dat unpack geoip -o /var/mosdns -f cn $v2dat_dir/geoip.dat
@@ -136,12 +160,16 @@ v2dat_dump() {
     fi
 }
 
+cloudflare_ip() {
+    uci -q get mosdns.config.cloudflare_ip
+}
+
 case $script_action in
     "dns")
         interface_dns
     ;;
-    "ad")
-        ad_block
+    "adlist")
+        get_adlist
     ;;
     "geodata")
         geodat_update && adlist_update && restart_service
@@ -150,16 +178,16 @@ case $script_action in
         logfile_path
     ;;
     "adlist_update")
-        adlist_update && restart_service
-    ;;
-    "ecs_remote")
-        ecs_remote
+        adlist_update && [ "$has_update" -eq 1 ] && restart_service
     ;;
     "flush")
         flush_cache
     ;;
     "v2dat_dump")
         v2dat_dump
+    ;;
+    "cloudflare")
+        cloudflare_ip
     ;;
     "version")
         mosdns version
