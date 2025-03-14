6f3c9423e65c442e34a17fdd2b1a6a0d96a2b528
coolsnowwolf <coolsnowwolf@gmail.com>
2025-03-15 07:59:12 +0800

    luci-app-softethervpn: update provided links for SoftEtherVPN clients

---
+++ applications/luci-app-softethervpn/luasrc/controller/softethervpn.lua
@@ -0,0 +1,20 @@
+module("luci.controller.softethervpn",package.seeall)
+
+function index()
+	if not nixio.fs.access("/etc/config/softethervpn") then
+		return
+	end
+	
+	entry({"admin", "vpn"}, firstchild(), "VPN", 45).dependent = false
+	local page = entry({"admin", "vpn", "softethervpn"}, cbi("softethervpn"), _("SoftEther VPN Service"), 50)
+	page.dependent = true
+	page.acl_depends = { "luci-app-softethervpn" }
+	entry({"admin", "vpn", "softethervpn", "status"}, call("act_status")).leaf = true
+end
+
+function act_status()
+	local e = {}
+	e.running = luci.sys.call("pidof vpnserver >/dev/null") == 0
+	luci.http.prepare_content("application/json")
+	luci.http.write_json(e)
+end
