5e47d8a15d1d1edb8b3f65431997d423e7a09f60
coolsnowwolf <31687149+coolsnowwolf@users.noreply.github.com>
2025-11-15 10:28:40 +0800

    luci-app-firewall: enable nat1 option High Performing Mode in fw4

--- applications/luci-app-firewall/htdocs/luci-static/resources/view/firewall/zones.js
+++ applications/luci-app-firewall/htdocs/luci-static/resources/view/firewall/zones.js
@@ -61,8 +61,7 @@ return view.extend({
 			o = s.option(form.ListValue, 'fullcone', _('Enable FullCone NAT'));
 			o.value("0", _("Disable"))
 			o.value("1", _("Compatible Mode"))
-			if (!fw4)
-				o.value("2", _("High Performing Mode"));
+			o.value("2", _("High Performing Mode"))
 			if (fw4)
 				o = s.option(form.Flag, 'fullcone6', _('Enable FullCone NAT6'));
 		}
