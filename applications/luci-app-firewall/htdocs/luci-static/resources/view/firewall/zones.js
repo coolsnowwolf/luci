6e175be1b12bb663530533dc46aa93c332c7319e
coolsnowwolf <coolsnowwolf@gmail.com>
2026-04-14 11:36:48 +0800

    luci-app-firewall: add NFT fullcone NAT1 support

--- applications/luci-app-firewall/htdocs/luci-static/resources/view/firewall/zones.js
+++ applications/luci-app-firewall/htdocs/luci-static/resources/view/firewall/zones.js
@@ -56,7 +56,11 @@ return view.extend({
 
 		o = s.option(form.Flag, 'drop_invalid', _('Drop invalid packets'));
 		if (L.hasSystemFeature('fullcone')) {
-			o = s.option(form.Flag, 'fullcone', _('Enable FullCone NAT'));
+			o = s.option(form.ListValue, 'fullcone', _('Enable FullCone NAT'));
+			o.value('0', _('Disable'));
+			o.value('1', _('FULLCONENAT'));
+			if (!fw4)
+				o.value('2', _('Boardcom Fullcone NAT1'));
 			if (fw4)
 				o = s.option(form.Flag, 'fullcone6', _('Enable FullCone NAT6'));
 		}
