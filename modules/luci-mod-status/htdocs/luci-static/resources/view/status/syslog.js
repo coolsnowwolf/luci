a52470b1e7d7607ccf0163e0384fa307dcd2799b
coolsnowwolf <coolsnowwolf@gmail.com>
2026-09-25 01:51:23 +0800

    luci-mod-status: fix syslog ACL with recent rpcd
    Recent rpcd releases re-authorize file operations against resolved symlink targets. Running the logread BusyBox symlink directly is therefore denied by the existing ACL. Use the upstream syslog wrapper so rpcd authorizes a real executable and log_file configurations remain supported.
    
    Fixes: coolsnowwolf/lede#14187
    Signed-off-by: coolsnowwolf <coolsnowwolf@gmail.com>

--- modules/luci-mod-status/htdocs/luci-static/resources/view/status/syslog.js
+++ modules/luci-mod-status/htdocs/luci-static/resources/view/status/syslog.js
@@ -6,12 +6,11 @@
 return view.extend({
 	load: function() {
 		return Promise.all([
-			L.resolveDefault(fs.stat('/sbin/logread'), null),
-			L.resolveDefault(fs.stat('/usr/sbin/logread'), null)
+			L.resolveDefault(fs.stat('/usr/libexec/syslog-wrapper'), null)
 		]).then(function(stat) {
-			var logger = stat[0] ? stat[0].path : stat[1] ? stat[1].path : null;
+			var logger = stat[0] ? stat[0].path : null;
 
-			return fs.exec_direct(logger, [ '-e', '^' ]).catch(function(err) {
+			return fs.exec_direct(logger).catch(function(err) {
 				ui.addNotification(null, E('p', {}, _('Unable to load log data: ' + err.message)));
 				return '';
 			});
